5a08aeeae3f0a8168d4d7d2e1898b436b01efc64
Eugene Korotaev <korotaev@sports.ru>
2020-01-21 21:34:58 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+    <router-view/>
+</template>
+
+<style lang="stylus">
+  @import "./assets/main.styl"
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="layout">
+    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  </div>
+</template>
+
+<script>
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+
+<style lang="stylus">
+  .layout {
+    width 100%
+    max-width 104rem
+    margin 0 auto
+    min-height 100vh
+    display flex
+    flex-direction column
+    background-image url('../assets/bg_desktop.png')
+    background-size cover
+  }
+</style>
